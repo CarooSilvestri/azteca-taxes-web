a548ba926003d7e89252f6a2ab6f3af151035cba
caroosilvestri <mcsilvestri19@gmail.com>
2023-04-15 10:03:50 -0300

    redy supp

---
+++ data/hardware.js
@@ -0,0 +1,311 @@
+var $hardware = [
+  {
+    "id": "1",
+    "name": "Dell Optiplex",
+    "type": "workstations",
+    "description": {
+      "overview": "The OptiPlex 3050 Micro Desktop is an ultra-compact business desktop with multiple mounting possibilities to best fit your workspace. Offers essential best-in-class security and manageability. Enhance your productivity with powerful workstations built for long hours of continuous use.",
+      "features": [
+        "Sturdy chassis with ample room for additional memory, storage and expansion cards",
+        "Ease of upgrading and service for years of use as your organization grows"
+      ]
+    },
+    "specs": [
+      {
+        "title": "Operating system",
+        "desc": "Microsoft Windows 10 Professional preinstalled"
+      },
+      {
+        "title": "Processor",
+        "desc": "6th Gen Intel® Core™ i3-6100T dual-core processor"
+      },
+      {
+        "title": "Memory",
+        "desc": "4GB high-bandwidth DDR4 RAM for basic multitasking"
+      },
+      {
+        "title": "Storage",
+        "desc": "500GB 7200 rpm SATA hard drive for serviceable file storage space"
+      },
+      {
+        "title": "Connectivity",
+        "desc": [
+          "4 SuperSpeed 10Gbs USB 3.1 ports for ultra-fast data transfer",
+          "2 USB 2.0 ports for quick plug-and-play connectivity",
+          "Built-in Gigabit Ethernet LAN port for wired network connectivity",
+          "Microphone and headphone jacks, HDMI output"
+        ]
+      },
+      {
+        "title": "Software",
+        "desc": "30-day trial of Microsoft Office included"
+      }
+    ],
+    "note": "Note: This computer does not include a built-in DVD/CD drive.",
+    "price": "$795"
+  },
+  {
+    "id": "2",
+    "name": "Lenovo Desktop",
+    "type": "workstations",
+    "description": {
+      "overview": "The Lenovo Desktop is a versatile desktop computer designed to handle multiple tasks with ease. Featuring up to 7th Gen Intel Core i7 processing and up to 512GB M.2 SATA SSD, this desktop delivers fast and responsive performance.",
+      "features": [
+        "Up to 8GB DDR4 RAM for smooth multitasking",
+        "Windows 10 Home for a familiar and productive computing experience"
+      ]
+    },
+    "specs": [],
+    "price": "$795"
+  },
+  {
+    "id": "3",
+    "name": "Lenovo Laptop",
+    "type": "laptops",
+    "description": {
+      "overview": "The Lenovo Laptop is a powerful and portable computing solution designed for productivity and entertainment on-the-go. Featuring up to 7th Gen Intel Core i7 processing and up to 512GB M.2 SATA SSD, this laptop delivers fast and responsive performance. The Lenovo Active Pen (sold separately) emulates the easy comfort of a pen-on-paper writing experience, and thanks to Windows Ink, you’ll discover all the ways you can turn thoughts into action.",
+      "features": [
+        "Up to 8GB DDR4 RAM for smooth multitasking",
+        "Windows 10 Home",
+        "Up to 512 GB M.2 SATA SSD",
+        "Up to 8 GB DDR4"
+      ]
+    },
+
+    "specs": [],
+    "price": "$795"
+  },
+  {
+    "id": "4",
+    "name": "ASUS Monitor 24″",
+    "type": "monitors",
+    "description": {
+      "overview": "This monitor features a 50,000,000:1 smart contrast ratio with aspect control to deliver clear, true-to-life images and video. The Splendid Video Intelligence technology allows you to choose from among 6 preset video modes for optimal viewing.",
+      "features": [
+        {
+          "title": "Ultrafast 2 ms response time",
+          "desc": "Allows pixels to change colors quickly to avoid streaking, blurring and ghosting in fast-moving scenes and video games."
+        },
+        {
+          "title": "23.6″ widescreen LED panel",
+          "desc": "Features a mercury-free design for safety."
+        },
+        {
+          "title": "50,000,000:1 smart contrast ratio and 300 cd/m² brightness",
+          "desc": "With aspect control to deliver clear, lifelike images."
+        },
+        {
+          "title": "1920 x 1080 true resolution",
+          "desc": "With 170° horizontal and 160° vertical viewing angles offers a clear, high-resolution picture from a variety of vantage points."
+        },
+        {
+          "title": "Splendid Video Intelligence technology",
+          "desc": "Offers 6 preset video modes (scenery, theater, game, night view, sRGB and standard) selected via a hot key to optimize video performance and image fidelity."
+        },
+        {
+          "title": "Hot key controls",
+          "desc": "Allow you to adjust the brightness and volume with an auto adjust option in addition to the video presets."
+        },
+        {
+          "title": "VESA standard 100mm x 100mm",
+          "desc": "Allows for wall mounting with optional mounting kit (not included)."
+        }
+      ]
+    },
+    "specs": [],
+    "price": "$795"
+  },
+  {
+    "id": "5",
+    "name": "Topaz T-L460 SigLite LCD 1×5 Series",
+    "type": "signature_pads",
+    "description": {
+      "overview": "SigLite LCD 1×5 (T-L460-HSB-R) is Topaz’s low-cost, pressure-sensitive electronic signature capture pad with an LCD display and USB connection.",
+      "features": []
+    },
+    "price": "$190"
+  },
+  {
+    "id": "6",
+    "name": "Topaz T-L460 SigLite 1×5 Series",
+    "type": "signature_pads",
+    "description": {
+      "overview": "With this model series, the signature is only viewable on the computer screen. If your application requires customers to view the signature on the pad’s screen, see the SigLite LCD 1×5.",
+      "features": []
+    },
+    "specs": [],
+    "price": "$125"
+  },
+  {
+    "id": "7",
+    "name": "Motorola-DS8108",
+    "type": "scanners",
+    "description": {
+      "overview": "We provide support for these certified handheld scanners, you are able to capture the taxpayer’s source documents and information to associate them with the appropriate tax return ATS tax Software. Get the power of a bar code scanner, digital camera and document scanner in a single, cost-effective device.",
+      "features": []
+    },
+    "specs": [],
+    "price": "$349.99"
+  }
+]
+
+
+$(document).ready(function() {
+  var contProds = document.getElementById("container")
+  var prods = document.getElementById("prods")
+  cargar_productos($hardware, contProds)
+  cargar_opciones($hardware, prods) 
+})
+
+function cargar_opciones(data, prods) {
+
+  $('.btn').click(function() { 
+
+    prods = document.getElementById("prods")      
+    filtrar(data, this.id, prods)
+    $('.btn').removeClass("active")
+    $(this).addClass("active")
+  })
+}
+
+function filtrar(data, tipo_prod="todos", container) { 
+  for (var clave in data) {
+    if (data[clave].type == tipo_prod) {
+      crear_titulo(container, tipo_prod)
+      crear_html(data[tipo_prod], container)
+    } 
+  }  
+}
+
+/* Cargar todos los productos sin filtrar */
+function cargar_productos(data, container) {
+
+  for (var clave in data) {
+     crear_html(data[clave], container)
+   } 
+ }
+ 
+/* Creación del HTML de los productos -formato- */
+function crear_html(prod, container) {
+
+  var card = document.createElement("div")
+  card.setAttribute("id", "card-producto")
+  card.setAttribute("class", "col-sm-12 col-md-3 col-xl-3")
+   
+  var img_cont = document.createElement("div")
+  img_cont.setAttribute("class", "cont-img")
+
+   var img_prod = set_img(prod)
+   img_cont.appendChild(img_prod)
+   card.appendChild(img_cont)
+
+  var texto = document.createElement("div")
+  texto.setAttribute("class", "text-card")
+
+  var titulo = document.createElement("h2")
+  texto.appendChild(titulo)
+
+   titulo.innerHTML += prod.name;
+      
+   var btn_prod = document.createElement("button")
+   btn_prod.innerText = "Ver más"
+   btn_prod.setAttribute("class", "btn-masprod")
+   btn_prod.setAttribute("id", "btn-masprod")
+   let pasar_prod = prod
+   btn_prod.addEventListener("click", (e) => {
+     
+     abrir_modal(pasar_prod) 
+   })
+
+   card.appendChild(texto)
+   card.appendChild(btn_prod)
+   container.appendChild(card)
+   
+}
+
+/* Formateo del modal */
+ function abrir_modal(elem) {
+
+   /* Obtengo los divs necesarios */
+   var modal = document.getElementById("modal")
+   var div_contenido = document.getElementById("modal-content")
+   var col_imagen = document.getElementById("col-modal-img")
+   var col_informacion = document.getElementById("col-modal-text")
+
+   /* Agrego imagen */
+   agregar_columna_imagen(elem, col_imagen)
+   
+   /* Agrego información */
+   crear_descripcion(elem, col_informacion)
+
+   /* Funcionamiento del modal */ 
+   modal.style.display = "block";
+
+   document.getElementsByClassName("close")[0].onclick = function() { 
+     modal.style.display = "none";
+   }
+     
+   window.onclick = function(event) {
+     if (event.target == modal) {
+       modal.style.display = "none";
+     }
+   }
+ }
+
+/* Agrego imagen */
+ function agregar_columna_imagen(elem, col_imagen) {
+
+   col_imagen.innerHTML=" "
+   var img = set_img(elem)
+   col_imagen.appendChild(img)
+ }
+
+/* Formateo de la info del modal */
+ function crear_descripcion(prod, col_informacion) {
+   
+   /* Reinicio contenido */
+   col_informacion.innerHTML = ""
+   
+   /* Agrego título */
+   var row_titulo = document.createElement("div")
+   row_titulo.setAttribute("class", "row")
+   titulo_modal(row_titulo, prod)
+   col_informacion.appendChild(row_titulo)
+
+   /* Armo la descripción del producto */  
+ }
+
+/*Agrego título del modal y el span del boton cerrado */
+ function titulo_modal (cont, prod) { 
+
+   var div_tit = document.createElement("div")
+   div_tit.setAttribute("id", "div_tit")
+   crear_titulo(div_tit, prod.name)
+   
+   /* Botón de cerrado */
+   var span = document.createElement("span")
+   span.setAttribute("class", "close")
+   span.innerHTML += "&times"
+   
+   div_tit.appendChild(span)
+   cont.appendChild(div_tit)
+ }
+
+ function set_img(prod) {
+
+  var img_prod = document.createElement("img")
+  img_prod.setAttribute("src", "rsc/"+ prod.name + " - " + prod.id +".jpg")
+  img_prod.setAttribute("width", "100%")  
+  img_prod.setAttribute("class", "img-fluid")
+  img_prod.setAttribute("alt", prod.name)
+  return img_prod
+}
+
+function crear_titulo(container, tipo) {
+    
+  container.innerHTML = ""
+  var titulo = document.createElement("h1")
+  titulo.setAttribute("class", "h1")
+  titulo.style.textTransform = "capitalize"
+  container.append(titulo)
+}
---
+++ data/productsInfo.js
@@ -0,0 +1,98 @@
+[
+  {
+    "id": "1",
+    "name": "ATS 1040 Desktop",
+    "tittles": [
+      {
+        "t": "Tax Software for High-Volume Tax Businesses",
+        "d": "ATS 1040 is the industry's leading tax software solution for high-volume tax businesses. Built based on the needs of busy tax offices that specialize in providing their taxpayer clients with fast and accurate tax returns, ATS 1040 has been a trusted software solution since 1989. With industry-leading tools such as Point-and-Shoot Error Correction™, exclusive TextLink™ integrated text messaging technology, and support for electronic signature pads and hand-held W-2 barcode scanners, ATS 1040 software users are able to process tax returns more quickly and accurately."
+      },
+      {
+        "t": "Fast, Easy, Efficient",
+        "d": "ATS 1040 is the fastest, easiest, and most efficient professional tax software solution for high-volume tax businesses that specialize in offering quick & accurate tax refunds and bank products. Built to speed processing of your clients’ tax returns and bank product refunds, ATS 1040 professional tax software helps you stay a step ahead of your competition.",
+        "points": [
+          "Work-in-Progress Screen for instant status of tax returns, IRS acknowledgements, rejects and bank product check prints.",
+          "Point-and-Shoot Error Correction™ takes you directly to errors for quick fixes",
+          "Multiple input modes including interview-based and forms-based",
+          "Multiple input modes including interview-based and forms-based",
+          "Multiple drains to the IRS each day",
+          "Real-time processing with Industry-Leading Banks"
+        ]
+      },
+      {
+        "t": "Industry-Leading Tools and Features",
+        "d": "ATS 1040 has been built with a host of industry-leading tools and features designed for high-volume professional tax preparation businesses specializing in bank products",
+        "points": [
+          "Zero-downtime, automatic software updates",
+          "Customizable reports for the needs of your high-volume tax business",
+          "Comprehensive diagnostics that check for tax return omissions and inconsistencies",
+          "And much more...!"
+        ]
+      }
+    ]
+  },
+  {
+    "id": "2",
+    "name": "ATS 1040 Online",
+    "tittles": [
+      {
+        "t": "Real-time, state-of-your-business updates at your fingertips",
+        "d": "Access instructional product videos and online training for the ATS Suite of professional tax preparation products and solutions. Online provides you with a superior solution that adapts and grows with your business – a single product that will support your tax preparation business every step of the way."
+      },
+      {
+        "t": "Designed to support tax offices of all types and sizes",
+        "d": "Get the latest information and requirements for registered tax return preparers, tax law changes, health care reform, Affordable Care Act requirements for businesses, and other useful Federal tax information. Simple enough for brand-new preparers, yet powerful enough for the most complex service bureau environments – ATS Online is built with your unique business in mind.",
+        "points": [
+          "Traditional tax offices",
+          "Mobile tax offices",
+          "Growing tax businesses",
+          "Multi-office businesses",
+          "Nationwide Service Bureaus"
+        ]
+      },
+      {
+        "t": "Industry-Leading Tools and Features",
+        "d": "ATS Online is a scalable and flexible solution that supports all Internet accessible devices and platforms.",
+        "points": [
+          "Windows",
+          "Mac OS",
+          "Mobile device",
+          "Tablet",
+          "Laptop",
+          "Desktop computer"
+        ]
+      }
+    ]
+  },
+  {
+    "id": "3",
+    "name": "ATS Corporations",
+    "description": "ATS Business tax software offers a complete solution for professional tax preparers who prepare Federal and State business tax returns.",
+    "tittles": [
+      {
+        "t": "100% Integrated with ATS 1040 Professional Tax Software",
+        "d": "",
+        "points": [
+          "Federal Business Solution",
+          "State Business Solution",
+          "Desktop Software Application",
+          "Management Dashboard",
+          "Work-In-Progress Screen",
+          "Point-and-Shoot Error Correction™",
+          "Forms-based Input",
+          "No-hold return transmission to IRS via Modernized e-File",
+          "Zero-downtime Software Updates",
+          "Electronic Signature Support",
+          "Permissions Based Administration",
+          "Scan & Store"
+        ]
+      }
+    ]
+  },
+  {
+    "id": "4",
+    "name": "ATS Partners",
+    "description": "Our Partnership Packages are designed for tax preparers who have not obtained their own EFIN. If you are new to the tax business or looking to branch off and start your own tax business this is a great package to start with. We offer 60/40, 70/30, 80/20 and 90/10 splits in favor of the partner with no EFIN required. Many of our current partners are former employees of H&R Block, Jackson Hewitt, Liberty Tax, and smaller tax companies looking to start their own tax preparation business.",
+    "tittles": []
+  }
+]
